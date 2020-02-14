9e7cc20d879b084a382961e1bfb7cada74e4a1a1
thc202 <thc202@gmail.com>
2020-02-14 15:58:06 +0000

    Tweak update-website action
    Use the workspace dir to resolve the directories (should fix the
    failure).
    Check if the pull request is still open (to updated it, force push).
    Ensure the required inputs are required.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- .github/actions/update-website/index.js
+++ .github/actions/update-website/index.js
@@ -3,10 +3,14 @@ const core = require('@actions/core')
 const exec = require('@actions/exec')
 const github = require('@actions/github')
 const io = require('@actions/io')
-const Octokit = require("@octokit/rest")
+const { Octokit } = require("@octokit/rest")
 
 async function git(dir, args, options) {
   return exec.exec('git', args, { ...{ cwd: dir, silent: true }, ...options })
+}
+
+function getRequiredInput(name) {
+  return core.getInput(name, { required: true })
 }
 
 async function run() {
@@ -14,9 +18,9 @@ async function run() {
     const owner = github.context.repo.owner
     const repo = github.context.repo.repo
 
-    const docsDir = path.resolve(__dirname, repo)
-    const websiteRepoName = core.getInput('repo')
-    const websiteDir = path.resolve(__dirname, websiteRepoName)
+    const docsDir = path.resolve(process.env.GITHUB_WORKSPACE, repo)
+    const websiteRepoName = getRequiredInput('repo')
+    const websiteDir = path.resolve(process.env.GITHUB_WORKSPACE, websiteRepoName)
     const websiteDocsDir = path.resolve(websiteDir, 'docs/api')
 
     await io.rmRF(websiteDocsDir)
@@ -27,11 +31,30 @@ async function run() {
       return
     }
 
-    const user = core.getInput('user')
-    const email = core.getInput('email')
+    const user = getRequiredInput('user')
+    const email = getRequiredInput('email')
     const authToken = process.env.AUTH_TOKEN
-    const gitHubBaseUrl = `https://${user}:${authToken}@github.com`
     const branch = 'update-api-docs'
+    const pullRequestParams = {
+      owner: owner,
+      repo: websiteRepoName,
+      base: getRequiredInput('branch'),
+      head: `${user}:${branch}`
+    }
+
+    const octokit = new Octokit({ auth: authToken })
+
+    const pulls = await octokit.pulls.list({
+      ...pullRequestParams,
+      state: 'open'
+    })
+
+    let pullId
+    if (pulls.data.length !== 0) {
+      pullId = pulls.data[0].id
+    }
+
+    const gitHubBaseUrl = `https://${user}:${authToken}@github.com`
     const title = 'Update API docs'
     const body = `From:\n${owner}/${repo}@${github.context.sha}`
     const commitMessage = `${title}\n\n${body}`
@@ -44,15 +67,20 @@ async function run() {
     await git(websiteDir, ['commit', '-sm', commitMessage])
     await git(websiteDir, ['push', '-f', 'origin', branch])
 
-    const octokit = new Octokit({ auth: authToken })
-    await octokit.pulls.create({
-      owner: owner,
-      repo: websiteRepoName,
-      base: core.getInput('branch'),
-      head: `${user}:${branch}`,
-      title: title,
-      body: body,
-    })
+    if (pullId) {
+      await octokit.pulls.update({
+        owner: owner,
+        repo: websiteRepoName,
+        pull_number: pullId,
+        body: body,
+      })
+    } else {
+      await octokit.pulls.create({
+        ...pullRequestParams,
+        title: title,
+        body: body,
+      })
+    }
 
   }
   catch (error) {
